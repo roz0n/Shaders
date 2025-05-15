42156cafb13127d0070a4e1a9381c281663c963a
Arnold Rozon <arnold@rozon.org>
2025-05-15 12:36:30 -0700

    Initial Commit

---
+++ Shaders/ShadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadersApp.swift
+//  Shaders
+//
+//  Created by Arnaldo Rozon on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ShadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
